afb7467d868ea7506e7f29e2ba0dfef82a8c1961
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-03-04 22:35:43 +0300

    Created second ABOUT block

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,15 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'title': ['Another Danger Slanted', 'system-ui'],
+      'desc':['BioRhyme','sans-serif'],
+    },
+    colors: {
+      button: "#AA966B",
+      reon: "#FBF9E8",
+      white:'#FFFFFF',
+    },
   },
   plugins: [],
 }
