6ec1ad6e5051d0f16fd9b32640804e3dcfca4ebc
Ivan Zaburdaev <ivanlom227@gmail.com>
2022-03-14 22:02:40 +0300

    Prepare for production

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,24 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import React, { useEffect } from "react";
+import Router from "next/router";
+
+// @ts-ignore
+import NProgress from "nprogress";
+
+//Binding events.
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
+  // useEffect config material-ui
+  useEffect(() => {
+    // Remove the server-side injected CSS.
+    const jssStyles = document.querySelector("#jss-server-side");
+    if (jssStyles && jssStyles.parentNode)
+      jssStyles.parentNode.removeChild(jssStyles);
+  }, []);
   return <Component {...pageProps} />
 }
 
